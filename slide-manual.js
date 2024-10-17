832e4f97f0db0ab1bc1636caa88f73e9ab4b81fa
iamyzddesign <134135287+iamyzddesign@users.noreply.github.com>
2024-10-17 22:49:02 +0700

    bisa nih

---
+++ slide-manual.js
@@ -0,0 +1,32 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    // Hide all slides
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Remove active class from all dots
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Display the selected slide and add active class to the current dot
+    slides[slideIndex - 1].style.display = "grid";
+    dots[slideIndex - 1].className += " active";
+}
