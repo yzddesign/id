62df7060983ba9190b07bd1c0a0802051b88f681
iamyzddesign <134135287+iamyzddesign@users.noreply.github.com>
2024-10-19 01:58:50 +0700

    ahayy

---
+++ showmore.js
@@ -0,0 +1,38 @@
+function showMore1() {
+    var hiddenCards = document.querySelectorAll('.hidden1');
+    var btnText = document.getElementById("showMoreBtn1");
+
+    hiddenCards.forEach((card, index) => {
+        // Show all hidden cards
+        card.classList.remove('hidden1');
+    });
+
+    // Change button text after showing all cards
+    btnText.style.display = 'none'; // Hide the button after cards are shown
+}
+
+function showMore3() {
+    var hiddenCards = document.querySelectorAll('.hidden3');
+    var btnText = document.getElementById("showMoreBtn3");
+
+    hiddenCards.forEach((card, index) => {
+        // Show all hidden cards
+        card.classList.remove('hidden3');
+    });
+
+    // Change button text after showing all cards
+    btnText.style.display = 'none'; // Hide the button after cards are shown
+}
+
+function showMore4() {
+    var hiddenCards = document.querySelectorAll('.hidden4');
+    var btnText = document.getElementById("showMoreBtn4");
+
+    hiddenCards.forEach((card, index) => {
+        // Show all hidden cards
+        card.classList.remove('hidden4');
+    });
+
+    // Change button text after showing all cards
+    btnText.style.display = 'none'; // Hide the button after cards are shown
+}
